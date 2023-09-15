b8c88cf8dda1705d3055ac9a4902206cdfe1fdcc
tom <ellington1209@gmail.com>
2023-09-15 19:37:38 -0300

    estalado componente loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import  Light  from "./themes/Ligth";
-import { Button, TextField, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from './routers/router';
+import { Provider } from "react-redux";
+
+
+
+
+import Light from "./themes/Ligth";
+import store from "./store/reducers/store";
+import { Loading } from "./components/Loading";
+
 
 
 const App = () => (
-  <ThemeProvider theme={Light}>   
-    <Button >aqui</Button>
-    <TextField fullWidth> nome</TextField>
-  </ThemeProvider>
- 
+  <Provider store={store}>
+    <ThemeProvider theme={Light}>
+      <Loading/>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>
+
 )
 export default App;
